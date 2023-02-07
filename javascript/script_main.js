ed535789743193df514f29a9df23f2330d9b6890
EmanuProds <emanuprods@proton.me~>
2023-02-07 13:53:31 -0300

    update

--- javascript/script_main.js
+++
@@ -1,5 +0,0 @@
-/*
-window.alert("Bem vindo ao meu site :D") // vai fazer uma pergunta
-window.confirm("Tomou um susto com o Pop-up? >.<") // janela com botão Ok e Cancelar
-window.prompt("Me informa seu nome por gentileza?")
-*/
